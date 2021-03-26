32dfefadcd0b4344a8a0c132f35ae84e1ef0b534
Hrezolit <Hrezolitaz@gmail.com>
2021-03-26 10:19:50 +0600

    practicing with UI elements

--- exercise 2.2/ViewController.swift
+++ exercise 2.2/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    var currentLight: Light = .yellow
+    var lightOn: CGFloat = 1
+    var lightOff: CGFloat = 0.3
+    
+    @IBOutlet var redImageView: UIImageView!
+    @IBOutlet var yellowImageView: UIImageView!
+    @IBOutlet var greenImageView: UIImageView!
+    @IBOutlet var startButton: UIButton!
+    
+    enum Light {
+        case red
+        case yellow
+        case green
     }
 
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        redImageView.alpha = lightOff
+        yellowImageView.alpha = lightOff
+        greenImageView.alpha = lightOff
 
+        redImageView.layer.cornerRadius = 50
+        yellowImageView.layer.cornerRadius = 50
+        greenImageView.layer.cornerRadius = 50
+        
+        
+    }
+
+    @IBAction func onOffAction() {
+        
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+            redImageView.alpha = lightOn
+            yellowImageView.alpha = lightOff
+            greenImageView.alpha = lightOff
+        case .yellow:
+            currentLight = .green
+            redImageView.alpha = lightOff
+            yellowImageView.alpha = lightOn
+            greenImageView.alpha = lightOff
+        case.green:
+            currentLight = .red
+            redImageView.alpha = lightOff
+            yellowImageView.alpha = lightOff
+            greenImageView.alpha = lightOn
+            
+        }
+    }
 }
 
